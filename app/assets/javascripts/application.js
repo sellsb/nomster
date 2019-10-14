564ffa470d226ab851e69f4ef90e3ff3dc3998eb
Bobby <sellsb@charter.net>
2019-10-14 00:26:09 +0000

    working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,11 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require popper
+//= require tether
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
-//= require_tree .
+
