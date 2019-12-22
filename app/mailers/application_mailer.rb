ad1e45a486562dc752625a6afedaeeaf31a11da6
Bobby <sellsbnash@gmail.com>
2019-12-22 23:25:40 +0000

    email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,10 +1,3 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
-def comment_added
   
- mail(to: "my@email.com",
-         subject: "A comment has been added to your place")
-  end
-
 end
